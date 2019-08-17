77a472f567d6c9f2f5786be21a2320c8a142af08
TatianePirico <tatiane.pirico@gmail.com>
2019-08-17 15:59:25 -0300

    Add tweet posting

--- src/App.js
+++ src/App.js
@@ -1,19 +1,90 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
+import Cabecalho from './components/Cabecalho'
+import NavMenu from './components/NavMenu'
+import Dashboard from './components/Dashboard'
+import Widget from './components/Widget'
+import TrendsArea from './components/TrendsArea'
+import Tweet from './components/Tweet'
 
-import Header from './components/Header';
-import NavBar from './components/NavBar';
+class App extends Component {
 
+	state = {
+		novoTweet: '',
+		listaTweet: []
+	};
 
-function App() {
-  return (
-    <Fragment>
+	novoTweetEstaValido() {
+		const novoTweetLength = this.state.novoTweet.length;
+		return novoTweetLength <= 140 && novoTweetLength > 0;
+	}
 
-      <Header>
-        <NavBar links={['Mensagens', 'Notificações', 'Coisas loucas']}/>
-      </Header>
-      
-    </Fragment>
-  );
+	handleCriaTweet = (event) => {
+		event.preventDefault();
+
+		this.setState({
+			listaTweet: [...this.state.listaTweet, this.state.novoTweet],
+			novoTweet: '',
+		});
+	}
+
+	render() {
+		return (
+			<Fragment>
+				<Cabecalho>
+					<NavMenu usuario="@omariosouto" />
+				</Cabecalho>
+				<div className="container">
+					<Dashboard>
+						<Widget>
+							<form className="novoTweet" onSubmit={this.handleCriaTweet}>
+								<div className="novoTweet__editorArea">
+									<span className={`novoTweet__status ${this.novoTweetEstaValido() ? '' : 'novoTweet__status--invalido'}`}>
+										{this.state.novoTweet.length}/140
+									</span>
+									<textarea 
+										className="novoTweet__editor" 
+										placeholder="O que está acontecendo?"
+										onChange={(event) => this.setState({novoTweet:event.target.value})}
+										value={this.state.novoTweet}
+									></textarea>
+								</div>
+								<button 
+									type="submit" 
+									className={`novoTweet__envia `}
+									disabled={!this.novoTweetEstaValido()}
+								>Tweetar</button>
+							</form>
+						</Widget>
+						<Widget>
+							<TrendsArea />
+						</Widget>
+					</Dashboard>
+					<Dashboard posicao="centro">
+						<Widget>
+							<div className="tweetsArea">
+
+								{
+									this.state.listaTweet.map((tweet, index) =>
+										<Tweet
+											key={`Tweet-${index}`}
+											nomeUsuario="Tatiane Pirico"
+											userName="@tatianepirico"
+											totalLikes={2}
+											avatarUrl={'https://stickershop.line-scdn.net/stickershop/v1/product/925/LINEStorePC/main.png;compress=true'}
+											>
+											<p className="tweet__conteudo">
+												<span>{tweet}</span>
+											</p>
+										</Tweet>
+									)
+								}
+							</div>
+						</Widget>
+					</Dashboard>
+				</div>
+			</Fragment>
+		);
+	}
 }
 
 export default App;
