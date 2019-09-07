f4c74a8a5a331bcc0e054a31909aa82691ef2223
TatianePirico <tatiane.pirico@gmail.com>
2019-09-07 09:08:40 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
 export default {
-  api: 'http://localhost:8080' //https://github.com/leticiacostadias/twitelum-api
+  //api: 'http://localhost:8080' //https://github.com/leticiacostadias/twitelum-api
+  api: 'https://twitelum-api.herokuapp.com'
 }
